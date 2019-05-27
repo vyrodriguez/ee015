2df938ef068fcb4ac44193a43a447376aea5fbb0
Daniel Ulloa <danielulloa1994@gmail.com>
2019-05-27 17:54:00 -0300

    agrega ej1

--- tp04/ej1/ej1.asm
+++ tp04/ej1/ej1.asm
@@ -4,11 +4,15 @@ ROM     EQU     $EC00
 Reset   EQU     $FFFE
 ;----------------VARIABLES---------------
         ORG     RAM
-
+VAR1    RMB     1
+VAR2    RMB     1
+VAR3    RMB     1
 ;-----------------PROCESO----------------
         ORG     ROM
-inicio      
-
+inicio  LDA     VAR1
+        EORA    VAR2
+        STA     VAR3
+        BRA     inicio
 ;----------------Reset------------------
         ORG     Reset
         FDB     inicio
