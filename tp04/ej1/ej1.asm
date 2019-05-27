6c6278dbcb14d3174da123ea32d5bc44ac644bfa
danielulloa <danielulloa1994@gmail.com>
2019-05-27 19:44:52 -0300

    ej1

--- tp04/ej1/ej1.asm
+++ tp04/ej1/ej1.asm
@@ -9,8 +9,10 @@ VAR2    RMB     1
 VAR3    RMB     1
 ;-----------------PROCESO----------------
         ORG     ROM
-inicio  LDA     VAR1
-        EORA    VAR2
+inicio  CLR     VAR2
+        CLR     VAR3
+        LDA     VAR1
+        EOR    VAR2
         STA     VAR3
         BRA     inicio
 ;----------------Reset------------------
