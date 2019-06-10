70ba5c8f368646c7e8929dc392ffbe100b9a7a6c
danielulloa <danielulloa1994@gmail.com>
2019-06-10 20:37:05 -0300

    ejercicios 01 02 04 05 corregidos

--- tp04/ej04/ej04.asm
+++ tp04/ej04/ej04.asm
@@ -1,4 +1,3 @@
-;----------------ETIQUETAS---------------
 RAM         EQU         $0080
 ROM         EQU         $EC00
 Reset       EQU         $FFFE
@@ -7,15 +6,15 @@ Reset       EQU         $FFFE
             ORG         RAM
 VAR1        RMB         1
 VAR2        RMB         1
-RESU        RMB         1
+VAR3        RMB         1
 
 ;-------------------ROM-------------------------
             ORG         ROM
-inicio      CLR         RESU
-            LDA         VAR1
-            EOR         VAR2
-            EOR         #$FF
-            STA         RESU
+inicio      LDA         VAR1
+            CMP         VAR2
+            BHS         escribir
+            LDA         VAR2
+escribir    STA         VAR3
             BRA         inicio
 
 ;-------------------Reset-------------------------
--- tp04/ej05/ej05.asm
+++ tp04/ej05/ej05.asm
@@ -1,4 +1,4 @@
-;----------------ETIQUETAS---------------
+;-------------------ETIQUETAS-------------------------
 RAM         EQU         $0080
 ROM         EQU         $EC00
 Reset       EQU         $FFFE
@@ -7,17 +7,15 @@ Reset       EQU         $FFFE
             ORG         RAM
 VAR1        RMB         1
 VAR2        RMB         1
-RESU        RMB         1
-
+VAR3        RMB         1
 ;-------------------ROM-------------------------
             ORG         ROM
-inicio      CLR         RESU
-            LDA         VAR1
-            EOR         VAR2
-            EOR         #$FF
-            STA         RESU
+inicio      LDA         VAR1
+            CMP         VAR2
+            BGE         mayor
+            LDA         VAR2
+mayor       STA         VAR3
             BRA         inicio
-
 ;-------------------Reset-------------------------
             ORG         Reset
             FDB         inicio
